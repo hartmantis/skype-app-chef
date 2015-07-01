2ccd55914d69bed2b0889198f3069e1a8d53c06e
Jonathan Hartman <j@p4nt5.com>
2015-06-30 23:22:08 -0700

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Skype App Cookbook CHANGELOG
+============================
+
+v0.0.1 (2015-06-30)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'skype-app'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures skype-app'
+long_description 'Installs/Configures skype-app'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
